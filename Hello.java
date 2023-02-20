e4b667e897b72c048a37d8c6aa5777ea7f4d5f9d
Gaurav1858 <124980123+Gaurav1858@users.noreply.github.com>
2023-02-20 12:56:21 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ import java.io.*;
 
 public class Hello {
    public static void main(String[] args){
-      system.out.println("Hello Guys!!! I'm Gaurav.");
+      System.out.println("Hello Guys!!! I'm Gaurav.");
    }
 }
