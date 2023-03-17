2710fbf36a3763891af21f7baf1842c1c9b6e839
Gaurav1858 <124980123+Gaurav1858@users.noreply.github.com>
2023-03-17 16:29:28 +0530

    updated

--- Hello.java
+++ Hello.java
@@ -5,7 +5,7 @@ public class Hello {
    public static void main(String[] args){
       System.out.println("Hello Guys!!! I'm Gaurav. My Employee ID is 1873.");
       System.out.println("I'm Kool");
-      System.out.println("hello2");
-      System.out.println("Helloooo");
+      //System.out.println("hello2");
+      //System.out.println("Helloooo");
    }
 }
