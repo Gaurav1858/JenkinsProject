746931c004b9871d62cd166a84f25ee4018250e6
Gaurav1858 <124980123+Gaurav1858@users.noreply.github.com>
2023-03-17 15:51:54 +0530

    Update Hello2.java

--- Hello2.java
+++ Hello2.java
@@ -2,7 +2,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Hello {
+public class Hello2 {
    public static void main(String[] args){
       System.out.println("Hello Guys!!! I'm in DevOps Studio.");
    }
